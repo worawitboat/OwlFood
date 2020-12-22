bb2ccaf4ec5d1c6abd6ea05d0af1cf8182615b84
worawitboat <worawit.pa.61@ubu.ac.th>
2020-12-22 21:08:36 +0700

    update 1

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -5,23 +5,100 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
+  final Map profile;
+  HomePage({Key key, @required this.profile}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState(profile);
 }
 
 class _HomePageState extends State<HomePage> {
-  
+  final profile;
+  _HomePageState(this.profile);
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'menu',
+    ),
+    Text(
+      'favorite menu',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   Widget build(BuildContext context) {
+    print(profile);
+
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: new EdgeInsets.only(top: 50.0),
+                    child: Container(
+                        width: 100.0,
+                        height: 100.0,
+                        decoration: new BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 3,
+                                blurRadius: 7,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                            image: new DecorationImage(
+                                fit: BoxFit.fill,
+                                image: new NetworkImage(
+                                    profile["picture"]["data"]["url"]))))),
+                Padding(
+                    padding: new EdgeInsets.only(top: 50.0, left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(profile["name"],
+                            style: TextStyle(
+                                fontFamily: "Prompt",
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)),
+                        Text("Member",
+                            style:
+                                TextStyle(fontFamily: "Prompt", fontSize: 15)),
+                      ],
+                    )),
+              ],
+            ),
+            Padding(
+                padding: new EdgeInsets.only(top: 50.0),
+                child: _widgetOptions.elementAt(_selectedIndex)),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Favorite',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/landingPage.dart
+++ lib/landingPage.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'homePage.dart';
 
 class LandingPage extends StatefulWidget {
   LandingPage({Key key}) : super(key: key);
@@ -25,43 +28,67 @@ class _LandingPageState extends State<LandingPage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[buildButtonFacebook(context)],
+          children: <Widget>[
+            SignInButton(
+              Buttons.Facebook,
+              text: "Login with facebook",
+              // mini: true,
+              onPressed: () {
+                loginWithFacebook(context);
+              },
+            )
+          ],
         ),
       ),
     );
   }
 
-  Widget buildButtonFacebook(BuildContext context) {
-    return InkWell(
-        child: Container(
-            constraints: BoxConstraints.expand(height: 50),
-            child: Text("Login with Facebook ",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.white)),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.blue[400]),
-            margin: EdgeInsets.only(top: 12),
-            padding: EdgeInsets.all(12)),
-        onTap: () => loginWithFacebook(context));
-  }
+  // Widget buildButtonFacebook(BuildContext context) {
+  //   return InkWell(
+  //       child: Container(
+  //           constraints: BoxConstraints.expand(height: 50),
+  //           child: Text("Login with Facebook ",
+  //               textAlign: TextAlign.center,
+  //               style: TextStyle(fontSize: 18, color: Colors.white)),
+  //           decoration: BoxDecoration(
+  //               borderRadius: BorderRadius.circular(16),
+  //               color: Colors.blue[400]),
+  //           margin: EdgeInsets.only(top: 12),
+  //           padding: EdgeInsets.all(12)),
+  //       onTap: () => loginWithFacebook(context));
+  // }
 
   Future loginWithFacebook(BuildContext context) async {
     FacebookLogin facebookLogin = FacebookLogin();
     FacebookLoginResult result = await facebookLogin
         .logInWithReadPermissions(['email', "public_profile"]);
 
-    String token = result.accessToken.token;
-    final graphResponse = await http.get(
-        'https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
-    final profile = JsonDecoder().convert(graphResponse.body);
-    print(profile);
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        String token = result.accessToken.token;
 
-    await _auth.signInWithCredential(
-        FacebookAuthProvider.getCredential(accessToken: token));
-    setState(() => {_isLoggedIn = false});
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,picture.width(100).height(100),email&access_token=${token}');
+        final profile = JsonDecoder().convert(graphResponse.body);
+        await _auth.signInWithCredential(
+            FacebookAuthProvider.getCredential(accessToken: token));
+        setState(() => _isLoggedIn = true);
 
-    checkAuth(context, profile); // after success, navigate to home.
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => HomePage(profile: profile)));
+        break;
+
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => _isLoggedIn = false);
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => _isLoggedIn = false);
+        break;
+    }
+
+    // checkAuth(context, profile); // after success, navigate to home.
   }
 
   Future checkAuth(BuildContext context, var profile) async {
